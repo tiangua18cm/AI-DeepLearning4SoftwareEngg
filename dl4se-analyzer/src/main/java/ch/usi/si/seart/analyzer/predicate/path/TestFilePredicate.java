8105af558a77e21d29c70c9679b0fc94b3c0b42e
tiangua18cm <tiangua18cm@proton.me>
2022-09-13 07:37:47 +0000

    added new words

---
+++ dl4se-analyzer/src/main/java/ch/usi/si/seart/analyzer/predicate/path/TestFilePredicate.java
@@ -0,0 +1,8 @@
+package ch.usi.si.seart.analyzer.predicate.path;
+
+import ch.usi.si.seart.treesitter.Language;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import 
