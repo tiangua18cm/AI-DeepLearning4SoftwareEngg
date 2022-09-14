f8ede78660453e160da9d2fb78ae829de04be8f9
tiangua18cm <tiangua18cm@proton.me>
2022-09-14 21:30:08 +0000

    ax: kweather   pts/2        Dec  2 13:27\t(colobus)

---
+++ dl4se-analyzer/src/test/java/ch/usi/si/seart/analyzer/BaseTest.java
@@ -0,0 +1,6 @@
+package ch.usi.si.seart.analyzer;
+
+import ch.usi.si.seart.treesitter.Language;
+import ch.usi.si.seart.treesitter.LibraryLoader;
+import ch.usi.si.seart.treesitter.Parser;
+import ch.usi.si.seart.treesitter.Tr
