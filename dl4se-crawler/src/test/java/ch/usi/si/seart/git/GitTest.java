e27cf29b8ad42348146facaf772a3051b77bcbcb
tiangua18cm <tiangua18cm@proton.me>
2022-10-15 16:25:24 +0000

    Version 1.1r45

--- dl4se-crawler/src/test/java/ch/usi/si/seart/git/GitTest.java
+++ dl4se-crawler/src/test/java/ch/usi/si/seart/git/GitTest.java
@@ -53,3 +53,87 @@ class GitTest {
     }
 
     @Test
+    void shallowCloneTest() throws IOException, GitException {
+        try (Git ignored = new Git(testRepoName, tmp, true)) {
+            checkContents(tmp.toFile());
+        }
+    }
+
+    @Test
+    void shallowCloneSinceTest() throws IOException, GitException {
+        try (Git ignored = new Git(testRepoName, tmp, LocalDateTime.of(2022, 2, 12, 0, 0))) {
+            checkContents(tmp.toFile());
+        }
+    }
+
+    private void checkContents(File dir) {
+        Assertions.assertTrue(dir.exists());
+        Assertions.assertTrue(dir.isDirectory());
+        File[] files = dir.listFiles();
+        Assertions.assertNotNull(files);
+        Assertions.assertEquals(4, files.length);
+    }
+
+    // ref: https://github.com/dabico/dl4se-crawler-test/commit/010e305c9818d7d8a985e91cf60739ac3b66d24e
+    @Test
+    void getLastCommitInfoTest() throws IOException, GitException {
+        try (Git git = new Git(testRepoName, tmp, true)) {
+            Git.Commit commit = git.getLastCommitInfo();
+            Assertions.assertEquals("010e305c9818d7d8a985e91cf60739ac3b66d24e", commit.getSha());
+            Assertions.assertEquals(LocalDateTime.of(2022, 2, 12, 20, 19, 51), commit.getTimestamp());
+        }
+    }
+
+    @Test
+    void getLastCommitEmptyRepoTest() throws IOException, GitException {
+        try (Git git = new Git(emptyRepoName, tmp)) {
+            Assertions.assertThrows(GitException.class, git::getLastCommitInfo);
+        }
+    }
+
+    @Test
+    void getDiffLowerBoundTest() throws IOException, GitException {
+        try (Git git = new Git(historyRepoName, tmp)) {
+            Git.Diff diff = git.getDiff("bc74b0bbd2821c4cdb0b1943f6b3afced8d49ca7");
+            // Expected diff output:
+            // M       .gitignore
+            // D       app.cpp
+            // A       app.py
+            // R079    app.java       dir/app.java
+            // R100    app.scala      dir/app.scala
+            // R100    app.c          program.c
+            Assertions.assertEquals(1, diff.getAdded().size());
+            Assertions.assertEquals(1, diff.getDeleted().size());
+            Assertions.assertEquals(1, diff.getModified().size());
+            Assertions.assertEquals(2, diff.getRenamed().size());
+            Assertions.assertEquals(1, diff.getEdited().size());
+        }
+    }
+
+    @Test
+    void getDiffTestLowerAndUpperBoundTest() throws IOException, GitException {
+        try (Git git = new Git(historyRepoName, tmp)) {
+            Git.Diff diff = git.getDiff(
+                    "bc74b0bbd2821c4cdb0b1943f6b3afced8d49ca7",
+                    "5dae826d5335bc633ce4bbae74e6b8394e563c13"
+            );
+            // Expected diff output:
+            // D       app.cpp
+            // A       app.py
+            // R100    app.c      program.c
+            Assertions.assertEquals(1, diff.getDeleted().size());
+            Assertions.assertEquals(1, diff.getAdded().size());
+            Assertions.assertEquals(0, diff.getModified().size());
+            Assertions.assertEquals(1, diff.getRenamed().size());
+            Assertions.assertEquals(0, diff.getEdited().size());
+            diff = git.getDiff(
+                    "db7dfcf4f141ffbf34c9acd089087e493029a973",
+                    "225c820cb9ba921127cfc57ee358e1205efd06c9"
+            );
+            // Expected diff output:
+            // R079    app.java    dir/app.java
+            // R100    app.c       program.c
+            Assertions.assertEquals(0, diff.getDeleted().size());
+            Assertions.assertEquals(0, diff.getAdded().size());
+            Assertions.assertEquals(0, diff.getModified().size());
+            Assertions.assertE
