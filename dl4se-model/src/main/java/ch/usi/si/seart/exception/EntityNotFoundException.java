39232115d38692878cfc5459ec0a4ece4e559c4e
tiangua18cm <tiangua18cm@proton.me>
2022-10-22 11:10:00 +0000

    Added files to ignore

---
+++ dl4se-model/src/main/java/ch/usi/si/seart/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package ch.usi.si.seart.exception;
+
+import lombok.AccessLevel;
+import lombok.experimental.StandardException;
+
+import javax.persistence.metamodel.Attribute;
+
+@StandardException(access = AccessLevel.PRIVATE)
+public abstract class
