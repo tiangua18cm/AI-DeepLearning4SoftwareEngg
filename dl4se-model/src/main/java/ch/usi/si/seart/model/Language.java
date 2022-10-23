e5a93d6fb6be02e8c36418f35e4aa66a9f431fa8
tiangua18cm <tiangua18cm@proton.me>
2022-10-23 12:49:11 +0000

    Ordenando

--- dl4se-model/src/main/java/ch/usi/si/seart/model/Language.java
+++ dl4se-model/src/main/java/ch/usi/si/seart/model/Language.java
@@ -21,4 +21,59 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persis
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "language")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@TypeDefs(@TypeDef(name = "list-array", typeClass = ListArrayType.class))
+public class Language {
+
+    @Id
+    @GeneratedValue
+    @JsonIgnore
+    Long id;
+
+    @LanguageName
+    @Column(unique = true)
+    String name;
+
+    @NotEmpty
+    @Type(type = "list-array")
+    @Column(columnDefinition = "text[]")
+    @Builder.Default
+    @ToString.Exclude
+    @JsonIgnore
+    List<@FileExtension String> extensions = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "languages")
+    @Builder.Default
+    @ToString.Exclude
+    @JsonIgnore
+    Set<GitRepo> repos = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Language language = (Language) o;
+        return id != null && Objects.equals(id, language.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
