3432067d9523189748ea6ffa325ded62968bdfc4
tiangua18cm <tiangua18cm@proton.me>
2022-10-28 12:12:45 +0000

    Delete index.rst

--- dl4se-model/src/main/java/ch/usi/si/seart/model/user/token/Token.java
+++ dl4se-model/src/main/java/ch/usi/si/seart/model/user/token/Token.java
@@ -22,4 +22,50 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-import javax.persiste
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Entity
+@Table(name = "token")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@FieldDefaults(level = AccessLevel.PROTECTED)
+public abstract class Token {
+
+    @Id
+    @GeneratedValue
+    @JsonIgnore
+    Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    String value;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @NotNull
+    LocalDateTime expires;
+
+    protected abstract LocalDateTime calculateExpiryDate();
+
+    @PreUpdate
+    @PrePersist
+    private void beforeSaving() {
+        expires = calculateExpiryDate();
+    }
+
+    public boolean isExpired() {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        return expires.isBefore(now);
+    }
+}
