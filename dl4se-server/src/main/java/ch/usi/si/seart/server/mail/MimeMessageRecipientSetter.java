2e16b9f904a2696e8e1e26505b763ec361df05ae
tiangua18cm <tiangua18cm@proton.me>
2022-11-06 07:11:07 +0000

    Docs fix.

---
+++ dl4se-server/src/main/java/ch/usi/si/seart/server/mail/MimeMessageRecipientSetter.java
@@ -0,0 +1,16 @@
+package ch.usi.si.seart.server.mail;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class MimeMessageRecipientSetter implements MimeMessagePropertySetter {
+
+    String recipient;
+
+    @O
