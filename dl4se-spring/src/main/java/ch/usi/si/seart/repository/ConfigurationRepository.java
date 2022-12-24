4b77b9cae67c8fb97bf7943999c440d458acc006
tiangua18cm <tiangua18cm@proton.me>
2022-12-24 09:45:25 +0000

    Fixed merge issues.

---
+++ dl4se-spring/src/main/java/ch/usi/si/seart/repository/ConfigurationRepository.java
@@ -0,0 +1,8 @@
+
+package ch.usi.si.seart.repository;
+
+import ch.usi.si.seart.model.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfigurationRepository extends JpaRepository<Configuration, String> {
+}
