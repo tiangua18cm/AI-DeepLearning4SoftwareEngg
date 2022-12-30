abfe42f4702e81a9cc7b37a291233255462f7c55
tiangua18cm <tiangua18cm@proton.me>
2022-12-30 19:07:40 +0000

    build all without copying dependencies

--- dl4se-transformer/src/main/java/ch/usi/si/seart/transformer/Transformer.java
+++ dl4se-transformer/src/main/java/ch/usi/si/seart/transformer/Transformer.java
@@ -10,4 +10,36 @@ import java.util.function.UnaryOperator;
 
 public interface Transformer extends UnaryOperator<String> {
 
-    String apply(String
+    String apply(String source);
+
+    default Language getLanguage() {
+        return null;
+    }
+
+    static Transformer identity() {
+        return string -> string;
+    }
+
+    static Transformer chain(Transformer... transformers) {
+        return chain(List.of(transformers));
+    }
+
+    static Transformer chain(Collection<Transformer> transformers) {
+        if (!canChain(transformers))
+            throw new IllegalArgumentException("Transformers operating on different languages can not be chained!");
+        return source -> {
+            Function<String, String> pipeline =  transformers.stream()
+                    .map(transformer -> (Function<String,String>) transformer)
+                    .reduce(Transformer.identity(), Function::andThen);
+            return pipeline.apply(source);
+        };
+    }
+
+    private static boolean canChain(Collection<Transformer> transformers) {
+        return 2 > transformers.stream()
+                .map(Transformer::getLanguage)
+                .filter(Objects::nonNull)
+                .distinct()
+                .count();
+    }
+}
---
+++ dl4se-transformer/src/main/java/ch/usi/si/seart/transformer/remover/PythonLineCommentRemover.java
@@ -0,0 +1,10 @@
+
+package ch.usi.si.seart.transformer.remover;
+
+public class PythonLineCommentRemover extends PythonNodeRemover {
+
+    @Override
+    protected String getPattern() {
+        return "(_ (comment) @target)";
+    }
+}
