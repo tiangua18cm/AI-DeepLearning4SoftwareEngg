2bc7267ee1347a4489718316bc299cf340ba9a52
tiangua18cm <tiangua18cm@proton.me>
2024-08-17 12:53:01 +0000

    autocommit with aec-process.pl 3246eae42ae7b17c7e20893bae0fa28f8bed5104

--- dl4se-website/src/views/ProfileView.vue
+++ dl4se-website/src/views/ProfileView.vue
@@ -50,4 +50,85 @@
                         <b-form-submit :disabled="!canUpdateUid">Save</b-form-submit>
                         <b-button type="button" @click="form.uid = generateUsername()">Random</b-button>
                       </div>
-      
+                    </b-form-group>
+                  </b-form-row>
+                </b-form>
+                <hr />
+                <b-card-sub-title sub-title-tag="h3" sub-title-text-variant="">Email</b-card-sub-title>
+                <b-form @submit.prevent.stop="updateEmail">
+                  <b-form-row>
+                    <b-form-group class="col">
+                      <b-input
+                        type="email"
+                        placeholder="New email"
+                        v-model.trim="form.email"
+                        :state="!v$.form.email.$invalid"
+                      />
+                    </b-form-group>
+                  </b-form-row>
+                  <b-form-row>
+                    <b-form-group class="col">
+                      <b-form-submit :disabled="!canUpdateEmail" :block="!$screen.sm">Change Email</b-form-submit>
+                    </b-form-group>
+                  </b-form-row>
+                </b-form>
+                <hr />
+                <b-card-sub-title sub-title-tag="h3" sub-title-text-variant="">Password</b-card-sub-title>
+                <b-form @submit.stop.prevent="updatePassword">
+                  <b-form-row>
+                    <b-form-group class="col">
+                      <b-input type="password" placeholder="New password" value="🗿🗿🗿🗿🗿🗿🗿🗿" disabled />
+                    </b-form-group>
+                  </b-form-row>
+                  <b-form-row>
+                    <b-form-group class="col">
+                      <b-form-submit :block="!$screen.sm">Change Password</b-form-submit>
+                    </b-form-group>
+                  </b-form-row>
+                </b-form>
+                <hr />
+                <b-card-sub-title sub-title-tag="h3" sub-title-text-variant="">Organisation</b-card-sub-title>
+                <b-form @submit.stop.prevent="updateOrganisation">
+                  <b-form-row>
+                    <!-- FIXME: We have to do this manually because the inner group div can not be customized -->
+                    <div role="group" class="form-group col">
+                      <div class="position-relative">
+                        <b-form-auto-complete
+                          type="text"
+                          v-model.trim="form.organisation"
+                          :server="organisationsURL"
+                          query-param="name"
+                          :debounce-time="250"
+                          :server-params="{ size: 5 }"
+                          :response-mapper="responseMapper"
+                          :state="!v$.form.organisation.$invalid"
+                        />
+                      </div>
+                    </div>
+                  </b-form-row>
+                  <b-form-row>
+                    <b-form-group class="col mb-0">
+                      <b-form-submit :disabled="!canUpdateOrganisation" :block="!$screen.sm">Save</b-form-submit>
+                    </b-form-group>
+                  </b-form-row>
+                </b-form>
+              </b-tab>
+            </b-tabs>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import getRandomName from "namesgenerator";
+import useVuelidate from "@vuelidate/core";
+import { email, required } from "@vuelidate/validators";
+import { uid } from "@/validators";
+import routerMixin from "@/mixins/routerMixin";
+import organisationsMixin from "@/mixins/organisationsMixin";
+import bootstrapMixin from "@/mixins/bootstrapMixin";
+import BFormSubmit from "@/components/FormSubmit";
+import BIconIdenticon from "@/components/IconIdenticon";
+import BFormAutoComplete from "@/componen
