04ec62a7c57f15539670588681ced5e40f3a1ef2
tiangua18cm <tiangua18cm@proton.me>
2024-08-24 08:49:51 +0000

    add entity

--- dl4se-website/src/views/RegisterView.vue
+++ dl4se-website/src/views/RegisterView.vue
@@ -25,4 +25,92 @@
         </b-form-row>
         <b-form-row>
           <b-form-group label-for="password">
-            
+            <template #label>
+              Password
+              <b-icon-asterisk font-scale="0.35" shift-v="16" class="text-danger" />
+            </template>
+            <b-form-input-password
+              id="password"
+              name="password"
+              autocomplete="new-password"
+              v-model.trim="form.password"
+              :disabled="submitted"
+              :state="v$.form.password.$dirty ? !v$.form.password.$invalid : null"
+            />
+            <b-form-text v-if="v$.form.password.$invalid">
+              <b-icon-exclamation-octagon />
+              <span>Must be at least 6 characters long, containing a number, uppercase and lowercase character.</span>
+            </b-form-text>
+          </b-form-group>
+        </b-form-row>
+        <b-form-row>
+          <b-form-group label-for="confirm">
+            <template #label>
+              Confirm Password
+              <b-icon-asterisk font-scale="0.35" shift-v="16" class="text-danger" />
+            </template>
+            <b-form-input-password
+              id="confirm"
+              name="confirm"
+              autocomplete="new-password"
+              v-model.trim="form.confirm"
+              :disabled="submitted"
+              :state="v$.form.confirm.$dirty ? !v$.form.confirm.$invalid : null"
+            />
+            <b-form-invalid-feedback v-if="v$.form.confirm.$dirty && v$.form.confirm.$invalid">
+              <b-icon-exclamation-triangle />
+              Must match the password above.
+            </b-form-invalid-feedback>
+          </b-form-group>
+        </b-form-row>
+        <b-form-row>
+          <!-- FIXME: We have to do this manually because the inner group div can not be customized -->
+          <div role="group" class="form-group">
+            <label for="organisation" class="d-block">
+              Organisation
+              <b-icon-asterisk font-scale="0.35" shift-v="16" class="text-danger" />
+            </label>
+            <div class="position-relative">
+              <b-form-auto-complete
+                id="organisation"
+                name="organisation"
+                type="text"
+                v-model.trim="form.organisation"
+                :server="organisationsURL"
+                query-param="name"
+                :debounce-time="250"
+                :server-params="{ size: 5 }"
+                :response-mapper="responseMapper"
+                :disabled="submitted"
+                :state="v$.form.organisation.$dirty ? !v$.form.organisation.$invalid : null"
+              />
+            </div>
+            <b-form-text v-if="v$.form.organisation.$invalid">
+              <b-icon-exclamation-octagon />
+              We use this information for analytics.
+            </b-form-text>
+          </div>
+        </b-form-row>
+        <b-form-row>
+          <b-form-group>
+            <b-form-text text-variant="dark">
+              <b-icon-asterisk font-scale="0.35" class="text-danger" />
+              Required fields
+            </b-form-text>
+          </b-form-group>
+        </b-form-row>
+        <b-form-row>
+          <b-form-group>
+            <b-form-submit :disabled="v$.$invalid || submitted" />
+          </b-form-group>
+        </b-form-row>
+        <b-overlay :show="submitted" variant="light" no-wrap :z-index="Number.MAX_SAFE_INTEGER" />
+      </b-form>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import useVuelidate from "@vuelidate/core";
+import { email, required, sameAs } from "@vuelidate/validators";
+import { pass
